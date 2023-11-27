cab96806ac0293710a0d6b287038da32583f1004
Bharath-boop <bharathboopathi51550@gmail.com>
2023-11-27 11:26:01 +0530

    task completed

---
+++ script.js
@@ -0,0 +1,35 @@
+const head = document.getElementById("head");
+setTimeout(() => {
+    head.innerText = "10"
+    setTimeout(() => {
+        head.innerText = "9"
+
+        setTimeout(() => {
+            head.innerText = "8"
+            setTimeout(() => {
+                head.innerText = "7"
+                setTimeout(() => {
+                    head.innerText = "6"
+                    setTimeout(() => {
+                        head.innerText = "5"
+                        setTimeout(() => {
+                            head.innerText = "4"
+                            setTimeout(() => {
+                                head.innerText = "3"
+                                setTimeout(() => {
+                                    head.innerText = "2"
+                                    setTimeout(() => {
+                                        head.innerText = "1"
+                                        setTimeout(() => {
+                                            head.innerText = "HAPPY INDEPENDENTS"
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
